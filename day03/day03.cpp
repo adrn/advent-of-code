d0cf6b73ed7e2aced334e77150709a0ef72174f9
Adrian Price-Whelan <adrian.prw@gmail.com>
2021-12-04 09:32:25 -0500

    checkpoint

--- day03/day03.cpp
+++ day03/day03.cpp
@@ -46,17 +46,21 @@ std::vector<bool> get_most_common(std::vector<std::vector<bool>> data) {
 
     std::vector<bool> most_common = {};
     for (int j=0; j < row_size; j++) {
-        if (counts[j] > num_rows/2)
+        if (counts[j] >= (num_rows / 2.0))
             most_common.push_back(true);
-        else if (counts[j] < num_rows/2)
+        else if (counts[j] < (num_rows / 2.0))
             most_common.push_back(false);
-        else
-            throw std::runtime_error("Both bits equally likely and I didn't account for that!");
     }
 
     return most_common;
 }
 
+std::vector<bool> get_least_common(std::vector<std::vector<bool>> data) {
+    auto most_common = get_most_common(data);
+    for (int i = 0; i < most_common.size(); i++)
+        most_common[i] = !most_common[i];
+    return most_common;
+}
 
 int binary_to_decimal(std::vector<bool> binary) {
     int num = 0;
@@ -84,6 +88,66 @@ void part1(std::vector<std::vector<bool>> data) {
 }
 
 
+void part2(std::vector<std::vector<bool>> data) {
+    std::vector<std::vector<bool> > data_copy = data;
+
+    int row_size = data[0].size();
+    std::vector<int> to_delete;
+
+    for (int j = 0; j < row_size; j++) {
+    // for (int j = 0; j < 3; j++) {
+        // auto most_common = get_most_common(data_copy);
+        auto most_common = get_least_common(data_copy);
+        to_delete = {};
+
+        std::cout << "most common: " << std::endl;
+        for (auto elem : most_common) {
+            std::cout << elem << " ";
+        }
+        std::cout << std::endl << std::endl;
+
+        for (int i = 0; i < data_copy.size(); i++) {
+            if (data_copy[i][j] != most_common[j]) {
+                std::cout << "i=" << i << "  j=" << j;
+                std::cout << "  data_copy=" << data_copy[i][j] << "  most_common=" << most_common[j] << std::endl;
+                to_delete.push_back(i);
+            }
+        }
+
+        for (int i = to_delete.size() - 1; i >= 0; i--) {
+            std::cout << "delete: " << to_delete[i] << std::endl;
+            if (data_copy.size() <= 1)
+                break;
+            data_copy.erase(data_copy.begin() + to_delete[i]);
+        }
+
+        for (auto vec : data_copy) {
+            for (auto elem : vec) {
+                std::cout << elem << " ";
+            }
+            std::cout << std::endl;
+        }
+        std::cout << "------" << std::endl << std::endl;
+
+        if (data_copy.size() <= 1)
+            break;
+    }
+
+    // for (auto elem : to_delete) {
+    //     std::cout << "to delete: " << elem << std::endl;
+    // }
+
+    // std::vector<bool> least_common = {};
+    // for (auto elem : most_common)
+    //     least_common.push_back(!elem);
+
+    // int gamma = binary_to_decimal(most_common);
+    // int epsilon = binary_to_decimal(least_common);
+
+    // std::cout << "Part 1 answer: " << gamma * epsilon << std::endl;
+}
+
+
 int main(int argc, char** argv) {
 
     if (argc != 2)
@@ -93,6 +157,7 @@ int main(int argc, char** argv) {
     std::string datafile_path(argv[1]);
     auto data = parse_file(datafile_path);
 
-    part1(data);
+    // part1(data);
+    part2(data);
 
 }
