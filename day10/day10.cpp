eacbe897f69243f57149bfcd20ea306426509b2b
Adrian Price-Whelan <adrian.prw@gmail.com>
2021-12-11 13:44:34 -0500

    part 2

--- day10/day10.cpp
+++ day10/day10.cpp
@@ -59,10 +59,14 @@ int main(int argc, char** argv) {
     // Part 1
     std::map<char, int> invalid_scores = {{')', 3}, {']', 57}, {'}', 1197}, {'>', 25137}};
 
-    std::vector<char> invalid;
+    std::vector<std::string> incomplete_lines = {};
+    bool is_corrupted;
+    std::vector<char> invalid = {};
     std::vector<char> queue;
     std::map<char, char> pairs = {{'(', ')'}, {'[', ']'}, {'{', '}'}, {'<', '>'}};
     for (auto &line : data) {
+        is_corrupted = false;
+        queue = {};
         for (auto &ch : line) {
             if (pairs.count(ch) == 1)
                 queue.push_back(ch);
@@ -70,13 +74,18 @@ int main(int argc, char** argv) {
                 if (ch == pairs[queue[queue.size()-1]])
                     queue.pop_back();
                 else {
-                    std::cout << "INVALID: Expected " << pairs[queue[queue.size()-1]] << " but found " << ch << std::endl;
+                    // std::cout << "INVALID: Expected " << pairs[queue[queue.size()-1]] << " but found " << ch << std::endl;
                     invalid.push_back(ch);
+                    is_corrupted = true;
                     break;
                 }
             }
         }
+        if ((queue.size() > 0) && !is_corrupted)
+            incomplete_lines.push_back(line);
     }
+
+    std::cout << data.size() << " " << incomplete_lines.size() << std::endl;
 
     int score = 0;
     for (auto &ch : invalid)
@@ -85,5 +94,44 @@ int main(int argc, char** argv) {
     std::cout << "Part 1: score = " << score << std::endl;
 
     std::cout << "--------" << std::endl;
-    // part2(data, minima);
+
+    // Part 2
+    std::map<char, int> complete_scores = {{')', 1}, {']', 2}, {'}', 3}, {'>', 4}};
+
+    std::vector<std::string> complete_sequences;
+    for (auto &line : incomplete_lines) {
+        queue = {};
+        for (auto &ch : line) {
+            if (pairs.count(ch) == 1)
+                queue.push_back(ch);
+            else {
+                if (ch == pairs[queue[queue.size()-1]])
+                    queue.pop_back();
+                else {
+                    queue.pop_back();
+                    complete_sequences.back() += pairs[queue[queue.size()-1]];
+                }
+            }
+        }
+
+        std::string seq = "";
+        for (auto &elem : queue)
+            seq = pairs[elem] + seq;
+        complete_sequences.push_back(seq);
+    }
+
+    std::vector<long> scores = {};
+    for (auto &seq : complete_sequences) {
+        long score = 0;
+        for (auto &ch : seq)
+            score = score * 5 + complete_scores[ch];
+        scores.push_back(score);
+    }
+    // print_vector1d(complete_sequences);
+    // print_vector1d(scores);
+    std::cout << "N scores: " << scores.size() << std::endl;
+
+    std::sort(scores.begin(), scores.end());
+    std::cout << "Part 2 score: " << scores[scores.size() / 2] << std::endl;
+
 }
